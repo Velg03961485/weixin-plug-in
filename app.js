d063ddeabf40879bcb79762455b4b90d7e651837
e <liuxu@yunfucloud.com>
2018-11-09 16:48:19 +0800

    velg first commmit

---
+++ app.js
@@ -0,0 +1,42 @@
+//app.js
+App({
+  onLaunch: function (options) {
+    //调用API从本地缓存中获取数据
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+    var _that = this;
+    wx: wx.getSystemInfo({
+      success: function (res) {
+        _that.systemInfo = res;
+      }
+    });
+  },
+  getUserInfo: function (cb) {
+    var that = this
+    if (this.globalData.userInfo) {
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    } else {
+      //调用登录接口
+      wx.login({
+        success: function () {
+          wx.getUserInfo({
+            success: function (res) {
+              that.globalData.userInfo = res.userInfo
+              typeof cb == "function" && cb(that.globalData.userInfo)
+            }
+          })
+        }
+      })
+    }
+  },
+  globalData: {
+    userInfo: null
+  },
+
+
+  /**
+   * 系统信息
+   */
+  systemInfo: null,//系统信息
+})
